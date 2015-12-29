395a0e7077d21f9c0ffe4a6a8fdd5f6409f0b4c6
Ziv Zamechek <ziv921@yahoo.com>
2015-12-28 20:06:34 -0500

    Changed new action in progiles controller

--- survelliance/app/controllers/profiles_controller.rb
+++ survelliance/app/controllers/profiles_controller.rb
@@ -2,8 +2,9 @@ class ProfilesController < ApplicationController
   def new
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
-    @profile = @user.build_profile
+    @profile = Profile.new
   end
+
   def create 
     @user = User.find( params[:user_id] )
     @profile = @user.build_profile(profile_params)
@@ -14,6 +15,7 @@ class ProfilesController < ApplicationController
       render action: :new
     end
   end
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
